3b15ac5a243a2fbd13ee5e80f740313b712dfc84
VladimirGostik <xgostik@stuba.sk>
2024-10-17 11:24:41 +0200

    Update tailwind.config.js to fix build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // alebo 'media' alebo 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // Remove or update the darkMode setting as needed
+  // darkMode: 'media', // or 'class' or remove entirely
   theme: {
     extend: {
       colors: {
-        customDark: '#06072D',  // Vlastná farba
+        customDark: '#06072D',  // Custom color
         customPurple: 'rgb(101, 85, 143)', 
         customPurpleNavbar: 'rgb(177, 166, 207)',
-        customSideBar: 'rgb(230, 215, 235)',  // Svetlejšia verzia
+        customSideBar: 'rgb(230, 215, 235)',  // Lighter version
         backroundPurple: 'rgb(131, 107, 194)',
       },
     },
@@ -16,6 +17,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
+    // Remove the line-clamp plugin, as it's included by default
+    // If you have other plugins, include them here
   ],
 };
