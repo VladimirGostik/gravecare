b61c50c093629b536a886e9bb7650d624234c8f8
VladimirGostik <xgostik@stuba.sk>
2024-10-17 09:46:53 +0200

    Build prepared

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,6 @@ module.exports = {
     '@babel/preset-react',
   ],
   plugins: [
-    '@babel/plugin-proposal-private-property-in-object'
+    '@babel/plugin-transform-private-property-in-object'
   ],
 };
